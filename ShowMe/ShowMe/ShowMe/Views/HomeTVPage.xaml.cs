573974e4f906c2fa13bfc13ee1e5b4e728ed5847
Laura <33421605+HennequinLaura@users.noreply.github.com>
2019-10-16 22:58:56 +0200

    Add Title to HomeTVPage

--- ShowMe/ShowMe/ShowMe/Views/HomeTVPage.xaml.cs
+++ ShowMe/ShowMe/ShowMe/Views/HomeTVPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShowMe.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace ShowMe.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomeTVPage : TabbedPage
     {
+        HomeTVViewModel viewModel;
         public HomeTVPage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new HomeTVViewModel();
         }
     }
 }
