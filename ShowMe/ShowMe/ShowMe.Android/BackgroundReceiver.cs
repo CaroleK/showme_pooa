9c330ab52f10527173d8174b3fcbffd1df6f2b7c
Laura <33421605+HennequinLaura@users.noreply.github.com>
2019-10-24 17:27:52 +0200

    Set up BackgroundReceiver and AlarmManager with dummy NotificationScheduler

---
+++ ShowMe/ShowMe/ShowMe.Android/BackgroundReceiver.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using Android.OS;
+using ShowMe.Services;
+
+namespace ShowMe.Droid
+{
+    [BroadcastReceiver]
+    public class BackgroundReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            PowerManager pm = (PowerManager)context.GetSystemService(Context.PowerService);
+            PowerManager.WakeLock wakeLock = pm.NewWakeLock(WakeLockFlags.Partial, "BackgroundReceiver");
+            wakeLock.Acquire();
+
+            NotificationScheduler.ScheduleNotification(); 
+
+            wakeLock.Release();
+        }
+    }
+}
---
+++ ShowMe/ShowMe/ShowMe/Services/NotificationScheduler.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShowMe.Services
+{
+    public class NotificationScheduler
+    {
+        static public void ScheduleNotification()
+        {
+            //TODO
+            CrossLocalNotifications.Current.Show("title", App.User.GivenName, 99, DateTime.Now);
+            CrossLocalNotifications.Current.Show("title", "body", 101, DateTime.Now.AddSeconds(10));
+        }
+    }
+}
