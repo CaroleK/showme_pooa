c9043f45cec0895deac90e1bc116a54ca236e66c
Laura <33421605+HennequinLaura@users.noreply.github.com>
2019-10-29 17:18:46 +0100

    Improve NotificationScheduler

--- ShowMe/ShowMe/ShowMe/Services/NotificationScheduler.cs
+++ ShowMe/ShowMe/ShowMe/Services/NotificationScheduler.cs
@@ -11,23 +11,29 @@ namespace ShowMe.Services
     public class NotificationScheduler
     {
         static public void ScheduleNotification(string userId)
-        {
-            TvMazeService service = new TvMazeService();
+        {            
+            TvMazeService service = new TvMazeService();            
+            DateTime DateTime = DateTime.Now;
+            CrossLocalNotifications.Current.Show("debug", "Woke up at " + DateTime.ToShortTimeString(), 0, DateTime);
 
             ObservableCollection<MyShow> myShows = new ObservableCollection<MyShow>(Task.Run(() => FireBaseHelper.GetUserShowList(userId)).Result);
-            DateTime DateTime = DateTime.Now;
-            // We check what's on TV in two days, to be able to schedule notifications for tomorrow
+
+            // We check what's on TV tomorrow and in two days, to be able to schedule notifications 24h before
+            string dateTimeTomorrow = DateTime.Year + "-" + DateTime.Month + "-" + (DateTime.Day + 1);
             string dateTimeAfterTomorrow = DateTime.Year + "-" + DateTime.Month + "-" + (DateTime.Day + 2);
             // Choice made to focus on US region because of the API's data 
             string regionISO = "US";
-            
+
+            List<ScheduleShow> sTomorrow = Task.Run(() => service.GetUpCommingEpisode(myShows, dateTimeAfterTomorrow, regionISO)).Result;
             List<ScheduleShow> sAfterTomorrow = Task.Run(() => service.GetUpCommingEpisode(myShows, dateTimeAfterTomorrow, regionISO)).Result;
-            foreach (ScheduleShow schedule in sAfterTomorrow)
+            sTomorrow.AddRange(sAfterTomorrow); 
+
+            foreach (ScheduleShow schedule in sTomorrow)
             {
                 if (GetByIdFromMyShows(myShows, schedule.Show.Id).MustNotify)
                 {
-                    string notificationBody = schedule.Show.Title + " - " + schedule.TitleEpisode + " on " + schedule.Show.Network.NetworkName + " at " + schedule.Airtime;
-                    string notificationTitle = "Don't miss it! Tomorrow on TV";
+                    string notificationBody = schedule.TitleEpisode + " -  on " + schedule.Show.Network.NetworkName + " at " + schedule.Airtime;
+                    string notificationTitle = "Don't miss it! " + schedule.Show.Title + "is on TV tomorrow";
                     string[] airtime = schedule.Airtime.Split(':');
                     DateTime notificationTime = new DateTime(DateTime.Year, DateTime.Month, DateTime.Day + 1, int.Parse(airtime[0]), int.Parse(airtime[1]), 0);
                     string notificationId = schedule.Show.Id + "" + DateTime.Day + "" + DateTime.Month;
