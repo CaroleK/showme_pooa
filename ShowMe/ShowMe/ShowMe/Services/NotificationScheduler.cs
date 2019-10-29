fc7d3e3b2d8395b3d3af5534e65aec41326c3ab6
Laura <33421605+HennequinLaura@users.noreply.github.com>
2019-10-29 19:05:46 +0100

    Prettify and comment NotificationScheduler class

--- ShowMe/ShowMe/ShowMe/Services/NotificationScheduler.cs
+++ ShowMe/ShowMe/ShowMe/Services/NotificationScheduler.cs
@@ -10,38 +10,61 @@ namespace ShowMe.Services
 {
     public class NotificationScheduler
     {
+        /// <summary>
+        /// Schedules notifications 24 hours before a show present in the user's list airs on US TV
+        /// </summary>
+        /// <param name="userId">The user id</param>
         static public void ScheduleNotification(string userId)
-        {            
-            TvMazeService service = new TvMazeService();            
+        {
             DateTime DateTime = DateTime.Now;
-            CrossLocalNotifications.Current.Show("debug", "Woke up at " + DateTime.ToShortTimeString(), 0, DateTime);
 
             ObservableCollection<MyShow> myShows = new ObservableCollection<MyShow>(Task.Run(() => FireBaseHelper.GetUserShowList(userId)).Result);
+            List<ScheduleShow> scheduledShows = RetrieveScheduledShows(myShows); 
+
+            foreach (ScheduleShow schedule in scheduledShows)
+            {
+                if (GetByIdFromMyShows(myShows, schedule.Show.Id).MustNotify)
+                {
+                    string notificationIdString = schedule.Show.Id + "" + DateTime.Day + "" + DateTime.Month;
+                    int notificationId = int.Parse(notificationIdString);
+
+                    string notificationBody = schedule.TitleEpisode + " -  on " + schedule.Show.Network.NetworkName + " at " + schedule.Airtime;
+                    string notificationTitle = "Don't miss it! " + schedule.Show.Title + "is on TV tomorrow";
+
+                    string[] airtime = schedule.Airtime.Split(':');
+                    DateTime notificationTime = new DateTime(DateTime.Year, DateTime.Month, DateTime.Day + 1, int.Parse(airtime[0]), int.Parse(airtime[1]), 0);
+                    
+                    CrossLocalNotifications.Current.Show(notificationTitle, notificationBody, notificationId, notificationTime);                    
+                }
+            }            
+        }
+
+        /// <summary>
+        /// Retrieves the schedule of relevant shows that are airing tomorrow and the day after in the US, to be able to pln notifications
+        /// </summary>
+        /// <param name="myShows">The user's shows list</param>
+        /// <returns>A list of ScheduleShow with airing details</returns>
+        static public List<ScheduleShow> RetrieveScheduledShows(ObservableCollection<MyShow> myShows)
+        {
+            TvMazeService service = new TvMazeService();
+            DateTime DateTime = DateTime.Now;
+
+            CrossLocalNotifications.Current.Show("debug", "Woke up at " + DateTime.ToShortTimeString(), 0, DateTime);
 
             // We check what's on TV tomorrow and in two days, to be able to schedule notifications 24h before
             string dateTimeTomorrow = DateTime.Year + "-" + DateTime.Month + "-" + (DateTime.Day + 1);
             string dateTimeAfterTomorrow = DateTime.Year + "-" + DateTime.Month + "-" + (DateTime.Day + 2);
+
             // Choice made to focus on US region because of the API's data 
             string regionISO = "US";
 
             List<ScheduleShow> sTomorrow = Task.Run(() => service.GetUpCommingEpisode(myShows, dateTimeAfterTomorrow, regionISO)).Result;
             List<ScheduleShow> sAfterTomorrow = Task.Run(() => service.GetUpCommingEpisode(myShows, dateTimeAfterTomorrow, regionISO)).Result;
-            sTomorrow.AddRange(sAfterTomorrow); 
+            sTomorrow.AddRange(sAfterTomorrow);
 
-            foreach (ScheduleShow schedule in sTomorrow)
-            {
-                if (GetByIdFromMyShows(myShows, schedule.Show.Id).MustNotify)
-                {
-                    string notificationBody = schedule.TitleEpisode + " -  on " + schedule.Show.Network.NetworkName + " at " + schedule.Airtime;
-                    string notificationTitle = "Don't miss it! " + schedule.Show.Title + "is on TV tomorrow";
-                    string[] airtime = schedule.Airtime.Split(':');
-                    DateTime notificationTime = new DateTime(DateTime.Year, DateTime.Month, DateTime.Day + 1, int.Parse(airtime[0]), int.Parse(airtime[1]), 0);
-                    string notificationId = schedule.Show.Id + "" + DateTime.Day + "" + DateTime.Month;
-
-                    CrossLocalNotifications.Current.Show(notificationTitle, notificationBody, int.Parse(notificationId), notificationTime);                    
-                }
-            }            
+            return sTomorrow; 
         }
+
 
         /// <summary>
         /// Retrieves a show MyShow from the Instance of the user's MyShow list by its ID
@@ -58,6 +81,7 @@ namespace ShowMe.Services
                 }
             }
 
+            // if no matching show was found, return null
             return null;
         }
     }
