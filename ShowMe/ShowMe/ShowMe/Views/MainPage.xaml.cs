4557d0bc32e937cc0bd550bc966422c1082a19e5
Laura <33421605+HennequinLaura@users.noreply.github.com>
2019-11-21 16:24:15 +0100

    Improve tab navigation logic

--- ShowMe/ShowMe/ShowMe/Views/MainPage.xaml.cs
+++ ShowMe/ShowMe/ShowMe/Views/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ShowMe.Models;
@@ -28,6 +29,31 @@ namespace ShowMe.Views
                          
         }
 
-      
+        // Both properties are useful to make navigation's logic smoother 
+        protected Page currentPage = null;
+        protected Page lastPage = null;
+
+        /// <summary>
+        /// Triggered when user navigates to new tab
+        /// Implements base.OnCurrentPageChanged()
+        /// Resets the tab the user just exited, in order to make navigation more intuitive 
+        /// </summary>
+        protected async override void OnCurrentPageChanged()
+        {
+            // Updates the last visited 
+            lastPage = currentPage;
+
+            // CurrentPage is the new tab the user just clicked on 
+            currentPage = CurrentPage;
+
+            // Resets the tab the user just exited to its root 
+            if (lastPage != null)
+            {
+                await lastPage.Navigation.PopToRootAsync();
+            }
+
+            base.OnCurrentPageChanged();
+        }
+
     }
 }
