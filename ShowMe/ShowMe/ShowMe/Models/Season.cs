319b852965f61dda1291a362539b49d63392d1f8
Laura <33421605+HennequinLaura@users.noreply.github.com>
2019-11-22 15:58:32 +0100

    Fix NextEpisode method

--- ShowMe/ShowMe/ShowMe/Models/Season.cs
+++ ShowMe/ShowMe/ShowMe/Models/Season.cs
@@ -21,23 +21,5 @@ namespace ShowMe.Models
         public int ? NumberOfEpisodes { get; set; }
 
         public List<Episode> EpisodesOfSeason { get; set; }
-
-        /// <summary>
-        /// In a list of seasons, find the object with the required season number
-        /// </summary>
-        /// <param name="seasonList">The list of seasons</param>
-        /// <param name="seasonNumber">The required season number</param>
-        /// <returns>The matching season object</returns>
-        static public Season FindSeasonBySeasonNumber(List<Season> seasonList, int seasonNumber)
-        {
-            foreach (Season s in seasonList)
-            {
-                if (s.Number == seasonNumber)
-                {
-                    return s;
-                }
-            }
-            return null; 
-        }
     }
 }
